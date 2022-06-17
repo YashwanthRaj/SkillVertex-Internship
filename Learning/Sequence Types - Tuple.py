f31820558a75be39f2d863f1d3cc368c671367c7
V Yashwanth Raj <99544533+YashwanthRaj@users.noreply.github.com>
2022-06-17 09:57:48 +0530

    Add files via upload

---
+++ Learning/Sequence Types - Tuple.py
@@ -0,0 +1,19 @@
+'''
+
+Tuple ()
+Sequence Datatype which can store multiple Datatype elements and it is ordered and unchangable
+HERE WE CAN'T EVEN ADD OR REMOVE ELEMENTS AS WE COULD IN SET
+
+'''
+
+my_tuple = ()
+print(type(my_tuple))
+
+my_tuple2 = (21,"Yashwanth" , "kanth", 54 , 2.02 , True)
+print(my_tuple2)
+print(type(my_tuple2))
+
+print(my_tuple2[:3]) # 0 to 2 position
+
+# Len used to determine the Length / size of a sequence Datatype
+print(len(my_tuple2)) # common to all Sequence datatypes
